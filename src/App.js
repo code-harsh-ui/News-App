e0be15164f3f11f74a89c3b728dcf04a618c61af
Harsh Jha <vampharsh96@gmail.com>
2022-06-30 12:59:25 +0530

    Components Sturcture and Adding props

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 
 
 import React, { Component } from 'react' // for class component
+import NavBar from './components/NavBar'; // for Navbar component
+import NewsContainer from './components/NewsContainer'; // for NewsContainer component
 
 // Here we are creating a basic class based component using the snippet "rcc"
 
 export default class App extends Component {
-  // We can also create a class variable and use it in the component using "this.variableName"
-  name = "Holden"
   render() {
     return (
      <>
-     <div>This is the first class based Component {this.name}</div>
+     {/* Here we are rendering both the components in web page */}
+     <NavBar/>
+     <NewsContainer/>
      </>
     )
   }
 }
+
+
+
