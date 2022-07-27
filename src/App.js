55086466dbede7b7c365fcb997c4f61c450a151d
Harsh Jha <vampharsh96@gmail.com>
2022-07-27 11:55:21 +0530

    Routing of Categories

--- src/App.js
+++ src/App.js
@@ -2,17 +2,63 @@ import './App.css';
 
 
 import React, { Component } from 'react'
-import NavBar from './components/NavBar'; 
+import NavBar from './components/NavBar';
 import NewsContainer from './components/NewsContainer';
+
+//*----------- Routing --------------
+// To use routing first we need install react-router-dom using the command 
+// >> "npm install react-router-dom" <<
+// Then we can import Router, Routes and Route to use routing
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-     <>
-     <NavBar/>
-     {/* //! Here we have used propTypes and defaultProps in NewsContainer component file that is why we have'nt used or passed any props here */}
-     <NewsContainer/>
-     </>
+      <>
+        {/* It's important wrap everythig in "Router" tag to use routing */}
+        <Router>
+          <NavBar />
+          {/* Then we'll use "Routes" this is an alternative of switch in new upgraded version of react */}
+          <Routes>
+            {/* //! In these route path if click there is no change in category it means routing will not remount the component because we have same component in all paths so we need to remount these forcefully when we clicked on any of these categories to do that we use "key" to identify the components uniquely
+            
+            or
+
+            //! we can just leave the anchor tag as it is in NavBar.js file where we used 'Link' and 'to'
+            
+            */}
+            <Route path="/"
+              element={<NewsContainer key='general' category='general'/>}
+            />
+            <Route path="/business"
+              element={<NewsContainer key='business' category='business'/>}
+            />
+            <Route path="/entertainment"
+              element={<NewsContainer key='entertainment' category='entertainment'/>}
+            />
+            <Route path="/general"
+              element={<NewsContainer key='general' category='general'/>}
+            />
+            <Route path="/health"
+              element={<NewsContainer key='health' category='health'/>}
+            />
+            <Route path="/science"
+              element={<NewsContainer key='science' category='science'/>}
+            />
+            <Route path="/sports"
+              element={<NewsContainer key='sports' category='sports'/>}
+            />
+            <Route path="/technology"
+              element={<NewsContainer key= 'technology' category='technology'/>}
+            />
+          </Routes>
+        </Router>
+      </>
     )
   }
 }
--- src/components/NewsContainer.js
+++ src/components/NewsContainer.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react'
 import Card from './Card'
 import Spinner from './Spinner'
-
-// Here we are using propTypes to set the default props and its type to do so first we need to import propTypes from prop-types using "impt" snippet
 import PropTypes from 'prop-types'
 
 
 export class NewsContainer extends Component {
 
-  //! alternative way of using propTypes and defaultProps with the help of "static variable"
-
-  // defaultProps is used when we forget to pass any props in the component "here we used this NewsContainer component" in "App.js file" with no props passed in it that is why this default props values is being used for NewsContainer component
+  // If there is no props passed in NewsContainer component this default props being used for the component
   static defaultProps = {
     country: 'in',
     pageSize: 9,
-    //!( we can use any of these categories to fetch news of these categories )
-    // category: 'business'
-    // category: 'entertainment'
-    // category: 'general'
-    // category: 'health'
-    category: 'science'
-    // category: 'sports'
-    // category: 'technology'
-
+    category: 'general'
   }
-
-  // Props types is used to set the types of props in this component it means if we defined this props as "string" we can't use the value in "number"
 
   static propTypes = {
     country: PropTypes.string,
@@ -45,10 +31,6 @@ export class NewsContainer extends Component {
   }
 
   async componentDidMount() {
-    // getting props country and pageSize from App.js file
-
-    //! Like country and apiKey we also have another request parameter i.e category (business, entertainment, general, health, science, sports, technology)
-
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=cfe6186e249941bab158966357415194&page=1&pageSize=${this.props.pageSize}`
     this.setState({ loading: true })
     let data = await fetch(url)
@@ -63,10 +45,6 @@ export class NewsContainer extends Component {
   }
 
   handleNextClick = async () => {
-    // getting props country and pageSize from App.js file
-
-    //! Like country and apiKey we also have another request parameter i.e category (business, entertainment, general, health, science, sports, technology)
-
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=cfe6186e249941bab158966357415194&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
     this.setState({ loading: true })
     let data = await fetch(url)
@@ -80,10 +58,6 @@ export class NewsContainer extends Component {
 
 
   handlePreviousClick = async () => {
-    // getting props country and pageSize from App.js file
-
-    //! Like country and apiKey we also have another request parameter i.e category (business, entertainment, general, health, science, sports, technology)
-
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=cfe6186e249941bab158966357415194&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
     this.setState({ loading: true })
     let data = await fetch(url)
@@ -131,19 +105,6 @@ export class NewsContainer extends Component {
     }
   }
 }
-
-//? or we can set propTypes in class component as well it means instead of using propTypes and defaultProps here we can place this in the class component using 'static variable'
-
-// NewsContainer.defaultProps = {
-//   country: 'in',
-//   pageSize: 6
-// }
-
-
-// NewsContainer.propTypes = {
-//   country: PropTypes.string,
-//   pageSize : PropTypes.number
-// }
 
 export default NewsContainer
 
